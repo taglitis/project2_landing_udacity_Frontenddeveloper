f5dc29270f132f1ab16e18a7b756a56ed9e39550
taglitis <boriskushnarev@gmail.com>
2020-09-27 15:56:26 +0600

    feat: smooth scrolling Changes color of clicked nav item. Reaches target smoothly.

--- js/app.js
+++ js/app.js
@@ -24,22 +24,86 @@
  * Start Helper Functions
  * 
 */
+
+
 const ul_element = document.querySelector('#navbar__list');
 let li_element, a_element, id_values;
 let section_elements = document.querySelectorAll('section');
-for(let i = 0; i < section_elements.length; i++){
-    id_values =  section_elements[i].getAttribute('id') 
-    console.log('i is ' + (i + 1));
-    console.log(section_elements[i].getAttribute('data-nav'));
-    console.log(id_values);
-    li_element = document.createElement('li');
-    a_element = document.createElement('a');
-    a_element.textContent = section_elements[i].getAttribute('data-nav');
-    a_element.setAttribute('href', '#'+id_values)
-    a_element.setAttribute('style', 'padding:10px; font-size:20px')
-    // li_element.appendChild(a_element);
-    ul_element.appendChild(li_element).appendChild(a_element);
-} 
+
+
+// function createNavig(){
+//     for(let i = 0; i < section_elements.length; i++){
+//         id_values =  section_elements[i].getAttribute('id') 
+//         // console.log('i is ' + (i + 1));
+//         // console.log(section_elements[i].getAttribute('data-nav'));
+//         // console.log(id_values);
+//         li_element = document.createElement('li');
+//         li_element.setAttribute('class', 'menu__link');
+//         a_element = document.createElement('a');
+//         a_element.textContent = section_elements[i].getAttribute('data-nav');
+//         a_element.setAttribute('href', '#'+id_values)
+//         // a_element.setAttribute('style', 'padding:10px; font-size:20px');
+//         ul_element.appendChild(li_element).appendChild(a_element);
+//     }
+// }
+
+
+// function scrollToSection(curretn_section){
+//     console.log(curretn_section)
+//     section_elements[i].scrollIntoView({behavior:"smooth", block:"start"});
+// }
+
+function createNavig(){
+    for(let i = 0; i < section_elements.length; i++){
+        id_values =  section_elements[i].getAttribute('id') 
+        li_element = document.createElement('li');
+        li_element.setAttribute('class', 'menu__link');
+        li_element.textContent = section_elements[i].getAttribute('data-nav');
+        console.log(section_elements[i].getAttribute('data-nav'));
+        ul_element.appendChild(li_element);        
+        li_element.addEventListener('click', function(){
+            section_elements[i].scrollIntoView({behavior:"smooth", block:"end"});
+            const current = document.getElementsByClassName("active");
+            if (current.length > 0){
+                current[0].className = current[0].className.replace(" active", "");
+            }   
+            this.className += " active";
+        }
+    )}
+}
+
+
+
+createNavig()
+
+
+// const li_navigation = document.getElementsByClassName("menu__link"); 
+// // console.log(section_elements[0]);
+// function eventListenNavClick(){
+//     for (var i = 0; i < li_navigation.length; i++){
+//         li_navigation[i].addEventListener('click', function(){
+//             // console.log(section_elements[i]);
+//             const current = document.getElementsByClassName("active");
+//             if (current.length > 0){
+//                 current[0].className = current[0].className.replace(" active", "");
+//             }   
+//             this.className += " active";
+//         })
+//     }
+// }    
+
+// eventListenNavClick()        
+        
+
+
+
+
+// function activateSection(){
+//     const scrollVert = window.pageYOffset;
+//     for(let i = 0; i < section_elements.length; i++){
+
+
+// }
 
 
 
@@ -72,5 +136,3 @@ for(let i = 0; i < section_elements.length; i++){
 // Scroll to section on link click
 
 // Set sections as active
-
-
