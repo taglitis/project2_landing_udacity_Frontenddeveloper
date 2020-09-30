6d00990b2ec62f72da9598a75b92d57e05758a3e
taglitis <boriskushnarev@gmail.com>
2020-09-30 21:47:39 +0600

    fix: change section color based nov item

--- js/app.js
+++ js/app.js
@@ -25,32 +25,40 @@
  * 
 */
 
-window.scrollTo(0,0);
+
 const ul_element = document.querySelector('#navbar__list');
 let li_element, a_element, id_values;
 let section_elements = document.querySelectorAll('section');
+// const ul_element = document.querySelector('#navbar__list');
+console.log(ul_element);
+const head_section = document.querySelector(".page__header");
+// section_elements[0].scrollIntoView();
 
 
 function createNavig(){
     for(let i = 0; i < section_elements.length; i++){
-        id_values =  section_elements[i].getAttribute('id') 
+        id_values =  section_elements[i].getAttribute('id');
+        // console.log(id_values);
         li_element = document.createElement('li');
         li_element.setAttribute('class', 'menu__link');
         li_element.textContent = section_elements[i].getAttribute('data-nav');
-        li_element.classList.add(id_values)
+        li_element.classList.add(id_values);
+        if (i == 0){li_element.classList.add('active');}
         ul_element.appendChild(li_element); 
         li_element.addEventListener('click', function() {
-            const head_section = document.querySelector(".page__header");
-            const current = document.getElementsByClassName("active");
+            // const head_section = document.querySelector(".page__header");
+            const current_li = document.getElementsByClassName("active");
+            const current_section = document.querySelector(".your-active-class");
+            const choose_section = document.querySelector("#"+this.classList[1]);
+            // console.log(current_section);
             const dimentions_section = section_elements[i].getBoundingClientRect();
             const dimentions_head_section = head_section.getBoundingClientRect();
-            console.log(dimentions_head_section.bottom, ', ', dimentions_section.top, ', ' , -dimentions_head_section.bottom + dimentions_section.top)
+            //console.log(dimentions_head_section.bottom, ', ', dimentions_section.top, ', ' , -dimentions_head_section.bottom + dimentions_section.top)
             window.scrollBy({top:(-dimentions_head_section.bottom + dimentions_section.top), left: dimentions_section.left, behavior:'smooth' });
-            if (current.length = 0){
-                // current[0].className = current[0].className.replace(" active", "");
-                this.className += " active";
-            }   
-            
+            current_li[0].className = current_li[0].className.replace(" active", "");
+            this.className += " active";  
+            current_section.className = current_section.classList.remove("your-active-class"); 
+            choose_section.classList.add("your-active-class");                
         })
     }
 }
@@ -59,45 +67,45 @@ function createNavig(){
 createNavig()
 
 
-/*Checks which part of the Page is in View, and changes its color*/
-/* Thanks to Toni's post where I got an idea how to get section dimentions*/
+// /*Checks which part of the Page is in View, and changes its color*/
+// /* Thanks to Toni's post where I got an idea how to get section dimentions*/
 
-function partInView () {
-    function InViewport(element) {
-        const dimentions = element.getBoundingClientRect();
-        const head_section = document.querySelector(".page__header");
-        const dimentions_head_section = head_section.getBoundingClientRect();
-        console.log('top', dimentions.top , (dimentions_head_section.top - (dimentions.bottom - dimentions.top)));
-        console.log('bottom' ,dimentions.bottom , window.innerHeight);
-        return (            
-            // dimentions.top >= (dimentions_head_section.top - (dimentions.bottom - dimentions.top)) && 
-            // dimentions.bottom <= window.innerHeight 
-            dimentions.top >= 0 &&
-            dimentions.left >= 0 &&
-            dimentions.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            dimentions.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
+// function partInView () {
+//     function InViewport(element) {
+//         const dimentions = element.getBoundingClientRect();
+//         const head_section = document.querySelector(".page__header");
+//         const dimentions_head_section = head_section.getBoundingClientRect();
+//         console.log('top', dimentions.top , (dimentions_head_section.top - (dimentions.bottom - dimentions.top)));
+//         console.log('bottom' ,dimentions.bottom , window.innerHeight);
+//         return (            
+//             // dimentions.top >= (dimentions_head_section.top - (dimentions.bottom - dimentions.top)) && 
+//             // dimentions.bottom <= window.innerHeight 
+//             dimentions.top >= 0 &&
+//             dimentions.left >= 0 &&
+//             dimentions.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+//             dimentions.right <= (window.innerWidth || document.documentElement.clientWidth)
+//         );
 
-    }    
+//     }    
 
 
-    for (let item of section_elements) {
-        window.addEventListener("scroll", function(event) {
-            li_local = document.querySelector('.'+item.id);
-            if (InViewport(item)) {
-            item.classList.add("your-active-class");
-            li_local.classList.add("active");
-            } else {
-            item.classList.remove("your-active-class");
-            li_local.classList.remove("active");
-        }
-        },
-        true
-        );
-    }
-}
+//     for (let item of section_elements) {
+//         window.addEventListener("scroll", function(event) {
+//             li_local = document.querySelector('.'+item.id);
+//             if (InViewport(item)) {
+//             item.classList.add("your-active-class");
+//             li_local.classList.add("active");
+//             } else {
+//             item.classList.remove("your-active-class");
+//             li_local.classList.remove("active");
+//         }
+//         },
+//         true
+//         );
+//     }
+// }
 
-partInView();
+// partInView();
 
 // eventListenNavClick()        
         
